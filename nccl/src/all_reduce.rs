b60d94e8fd77afa29c359cb83cf42171d99bbb98
YdrMaster <ydrml@hotmail.com>
2024-04-15 19:24:33 +0800

    test(nccl): 为 All Reduce 实现一个单元测试
    Signed-off-by: YdrMaster <ydrml@hotmail.com>

--- nccl/src/all_reduce.rs
+++ nccl/src/all_reduce.rs
@@ -28,3 +28,51 @@ impl Communicator {
         ));
     }
 }
+
+#[test]
+fn test() {
+    use cuda::ContextResource;
+    use std::iter::zip;
+
+    const N: usize = 10 << 10; // 10K * sizeof::<f32>() = 40K bytes
+
+    cuda::init();
+
+    let mut array = [1.0f32; N];
+    let group = crate::CommunicatorGroup::new(&[0, 1]);
+    let contexts = group.contexts().collect::<Vec<_>>();
+    let mut streams = contexts
+        .iter()
+        .map(|context| context.apply(|ctx| ctx.stream().sporulate()))
+        .collect::<Vec<_>>();
+    let mem = group
+        .call()
+        .iter()
+        .enumerate()
+        .map(|(i, comm)| {
+            contexts[i].apply(|ctx| {
+                let stream = unsafe { ctx.sprout(&streams[i]) };
+                let mut mem = stream.from_host(&array);
+                comm.all_reduce(
+                    &mut mem,
+                    None,
+                    CudaDataType::f32,
+                    ReduceType::ncclSum,
+                    &stream,
+                );
+                mem.sporulate()
+            })
+        })
+        .collect::<Vec<_>>();
+
+    for (i, (context, mut mem)) in zip(contexts, mem).enumerate() {
+        context.apply(|ctx| {
+            ctx.synchronize();
+            cuda::memcpy_d2h(&mut array, unsafe { &*ctx.sprout(&mem) });
+            assert_eq!(array, [2.; N]);
+
+            unsafe { ctx.kill(&mut mem) };
+            unsafe { ctx.kill(&mut streams[i]) };
+        });
+    }
+}
