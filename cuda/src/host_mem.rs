2b3bf42157320ea0e51e51f3ceeddc1167861130
YdrMaster <ydrml@hotmail.com>
2024-10-14 13:31:39 +0800

    bench: 添加一个锁页传输的性能测试
    Signed-off-by: YdrMaster <ydrml@hotmail.com>

--- cuda/src/host_mem.rs
+++ cuda/src/host_mem.rs
@@ -80,3 +80,68 @@ fn test_behavior() {
     ptr = null_mut();
     driver!(cuMemFreeHost(ptr));
 }
+
+#[test]
+fn bench() {
+    use rand::Rng;
+    use std::time::{Duration, Instant};
+
+    if let Err(crate::NoDevice) = crate::init() {
+        return;
+    }
+    crate::Device::new(0).context().apply(|ctx| {
+        let mut pagable = vec![0.0f32; 256 << 20];
+        rand::thread_rng().fill(&mut *pagable);
+        let pagable = unsafe {
+            from_raw_parts(
+                pagable.as_ptr().cast::<u8>() as *const u8,
+                size_of_val(&*pagable),
+            )
+        };
+
+        let size = pagable.len();
+
+        let mut locked = ctx.malloc_host::<u8>(size);
+        locked.copy_from_slice(&pagable);
+
+        let stream = ctx.stream();
+        let mut dev = ctx.malloc::<u8>(size);
+
+        fn bench_memcpy(
+            host: &[u8],
+            dev: &mut [crate::DevByte],
+            stream: &crate::Stream,
+        ) -> (Duration, Duration) {
+            let time = Instant::now();
+            let start = stream.record();
+            stream.memcpy_h2d(dev, host);
+            let end = stream.record();
+            let host = time.elapsed();
+            end.synchronize();
+            let dev = end.elapse_from(&start);
+            (host, dev)
+        }
+        fn format_bw(gb: f32, dur: Duration) -> String {
+            format!("{:.3}gb/s", gb / dur.as_secs_f32())
+        }
+
+        println!(
+            "{:^10} | {:^10} | {:^10} | {:^10} | {:^10} | {:^10}",
+            "sync host", "sync dev", "sync bw", "async host", "async dev", "async bw"
+        );
+        let gb = size as f32 / (1 << 30) as f32;
+        for _ in 0..10 {
+            let (sync_host, sync_dev) = bench_memcpy(&pagable, &mut dev, &stream);
+            let (async_host, async_dev) = bench_memcpy(&locked, &mut dev, &stream);
+            println!(
+                "{:^10.3?} | {:^10.3?} | {:^10} | {:^10.3?} | {:^10.3?} | {:^10}",
+                sync_host,
+                sync_dev,
+                format_bw(gb, sync_dev),
+                async_host,
+                async_dev,
+                format_bw(gb, async_dev),
+            );
+        }
+    });
+}
