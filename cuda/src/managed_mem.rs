a6e97b93be9a636d0e62ba1294d11f6d28f1081a
cearX <xhclear@outlook.com>
2025-03-19 17:19:44 +0800

    add test_behavior_multi_stream_async_access

--- cuda/src/managed_mem.rs
+++ cuda/src/managed_mem.rs
@@ -1,4 +1,4 @@
-use crate::{Blob, CurrentCtx, bindings::CUdeviceptr};
+use crate::{Blob, CurrentCtx, Stream, bindings::CUdeviceptr};
 use context_spore::{AsRaw, impl_spore};
 use std::{
     alloc::Layout,
@@ -35,13 +35,42 @@ pub fn _memcpy_d2d_man(dst: &mut [ManByte], src: &[ManByte]) {
     driver!(cuMemcpyDtoD_v2(dst.as_ptr() as _, src.as_ptr() as _, len))
 }
 
+impl Stream<'_> {
+    #[inline]
+    pub fn memcpy_h2d_man_stream<T: Copy>(&self, dst: &mut [ManByte], src: &[T]) {
+        let len = size_of_val(src);
+        let src = src.as_ptr().cast();
+        assert_eq!(len, size_of_val(dst));
+        driver!(cuMemcpyHtoDAsync_v2(
+            dst.as_ptr() as _,
+            src,
+            len,
+            self.as_raw()
+        ))
+    }
+
+    #[inline]
+    pub fn memcpy_d2h_man_stream<T: Copy>(&self, dst: &mut [T], src: &[ManByte]) {
+        let len = size_of_val(dst);
+        let dst = dst.as_mut_ptr().cast();
+        assert_eq!(len, size_of_val(src));
+        driver!(cuMemcpyDtoHAsync_v2(
+            dst,
+            src.as_ptr() as _,
+            len,
+            self.as_raw()
+        ))
+    }
+}
+
 impl_spore!(ManMem and ManMemSpore by (CurrentCtx, Blob<CUdeviceptr>));
 
 impl CurrentCtx {
     pub fn malloc_managed<T: Copy>(&self, len: usize) -> ManMem<'_> {
         let len = Layout::array::<T>(len).unwrap().size();
         let mut ptr = 0;
-        let flags = 0x1; // CUmemAttachflags: GLOBAL = 0x1, HOST = 0x2, SINGLE = 0x4;
+        // CUmemAttachflags, should be 0x1 or 0x2; GLOBAL = 0x1, HOST = 0x2; Global: Memory can be accessed by any stream on any device; Host: Memory cannot be accessed by any stream on any device
+        let flags = 0x1;
         if len != 0 {
             driver!(cuMemAllocManaged(&mut ptr, len, flags));
         }
@@ -142,4 +171,64 @@ fn test_managed() {
 
         assert_eq!(pagable, pagable2);
     });
+}
+
+#[test]
+fn test_behavior_multi_stream_async_access() {
+    use rand::Rng;
+
+    if let Err(crate::NoDevice) = crate::init() {
+        return;
+    }
+
+    let dev = crate::Device::new(0);
+    dev.context().apply(|ctx| {
+        let mut pagable = vec![0.0f32; 256 << 20];
+        rand::rng().fill(&mut *pagable);
+        let pagable = unsafe {
+            from_raw_parts(
+                pagable.as_ptr().cast::<u8>() as *const u8,
+                size_of_val(&*pagable),
+            )
+        };
+
+        let mut pagable2 = vec![0.0f32; 256 << 20];
+        let pagable2 = unsafe {
+            from_raw_parts_mut(
+                pagable2.as_mut_ptr().cast::<u8>() as *mut u8,
+                size_of_val(&*pagable2),
+            )
+        };
+
+        let mut pagable3 = vec![0.0f32; 256 << 20];
+        let pagable3 = unsafe {
+            from_raw_parts_mut(
+                pagable3.as_mut_ptr().cast::<u8>() as *mut u8,
+                size_of_val(&*pagable3),
+            )
+        };
+
+        let size = pagable.len();
+        let mut man = ctx.malloc_managed::<u8>(size);
+
+        let stream0 = ctx.stream();
+        let stream1 = ctx.stream();
+        let stream2 = ctx.stream();
+
+        stream0.memcpy_h2d_man_stream(&mut man, pagable);
+        let event = stream0.record();
+
+        stream2.memcpy_d2h_man_stream(pagable3, &man);
+
+        stream1.wait_for(&event);
+        stream1.memcpy_d2h_man_stream(pagable2, &man);
+
+        assert_eq!(pagable, pagable2);
+        assert_eq!(pagable, pagable3); // 在host上检验时会等待device同步, 检测不出来stream2未使用event进行同步操作
+    });
+}
+
+#[test]
+fn test_behavior_multi_context_access() {
+    todo!()
 }
