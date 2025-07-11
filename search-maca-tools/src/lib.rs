c2b12d3cb6609ef86e7d808c9a06aa1407b9be67
YdrMaster <ydrml@hotmail.com>
2025-07-11 10:42:05 +0800

    feat: 搜索工具兼容两种沐曦环境
    Signed-off-by: YdrMaster <ydrml@hotmail.com>

--- search-maca-tools/src/lib.rs
+++ search-maca-tools/src/lib.rs
@@ -9,14 +9,32 @@ pub fn find_maca_root() -> Option<PathBuf> {
     env::var_os("MACA_PATH").map(PathBuf::from)
 }
 
-pub fn include_maca(path: impl AsRef<Path>) {
+#[derive(Clone, Copy, Debug)]
+#[repr(u8)]
+pub enum MacaType {
+    MX,
+    HT,
+}
+
+pub fn include_maca(path: impl AsRef<Path>) -> Option<MacaType> {
     if env::var_os("DOCS_RS").is_some() || cfg!(doc) {
-        return;
+        return None;
     }
-    let lib = path.as_ref().join("lib");
-    println!("cargo:rustc-link-search={}", lib.display());
-    println!("cargo:rustc-link-lib=dylib=hcruntime");
-    println!("cargo:rustc-link-lib=dylib=htc-runtime64")
+    let path = path.as_ref();
+    println!("cargo:rustc-link-search={}", path.join("lib").display());
+
+    let bin = path.join("mxgpu_llvm/bin");
+    if bin.join("mxcc").is_file() {
+        println!("cargo:rustc-link-lib=dylib=mcruntime");
+        println!("cargo:rustc-link-lib=dylib=mxc-runtime64");
+        Some(MacaType::MX)
+    } else if bin.join("hpcc").is_file() {
+        println!("cargo:rustc-link-lib=dylib=hcruntime");
+        println!("cargo:rustc-link-lib=dylib=htc-runtime64");
+        Some(MacaType::HT)
+    } else {
+        panic!()
+    }
 }
 
 #[test]
